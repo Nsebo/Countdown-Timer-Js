ad759ce22531ef3361ceafa8764dff2f56626773
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-03-07 21:36:28 +0100

    still struggling with making the count down work :(

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var cdTimer = new Date('Mar 20, 2020 00:00:00').getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = cdTimer - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById(
+    'counter'
+  ).innerHTML = `<div>${days}<span>Days</span></div>
+    <div>${hours}<span>Hours</span></div>
+    <div>${minutes}<span>Minutes</span></div>
+    <div>${seconds}<span>Seconds</span></div>`;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('counter').innerHTML = 'ATHLETICS TIME!!';
+  }
+});
